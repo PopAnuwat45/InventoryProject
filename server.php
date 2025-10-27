48210b54b5a1e15e2d1ea8aa2c59b3eceb37ddfe
PopAnuwat45 <awpn8283@gmail.com>
2025-10-28 00:21:44 +0700

    Create server.php

---
+++ server.php
@@ -0,0 +1,9 @@
+<?php
+        // connect to DB
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "electro_shop_db";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
